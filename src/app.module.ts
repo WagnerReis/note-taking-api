1d168dc8ebab3ac38dc6c014be9e155e25979534
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-17 18:33:28 -0300

    feat: add MongoDB and configuration setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './modules/env/env';
+import { EnvService } from './modules/env/env.service';
+import { EnvModule } from './modules/env/env.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRootAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      useFactory: (envService: EnvService) => ({
+        uri: envService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [EnvService],
 })
 export class AppModule {}
