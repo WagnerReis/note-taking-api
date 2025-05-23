1c1843b0e75be9fa5ff6996eb50e0e5ef285cd00
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-23 11:17:06 -0300

    refactor(users): update MongoCreateUserMapper and User model

--- src/modules/users/mappers/mongo-create-user.mapper.ts
+++ src/modules/users/mappers/mongo-create-user.mapper.ts
@@ -1,13 +1,23 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { User } from '../entities/user.entity';
 import { User as UserModel } from '../models/user.model';
 
 export class MongoCreateUserMapper {
   static toDomain(raw: UserModel): User {
-    return User.create(raw);
+    return User.create(
+      {
+        name: raw.name,
+        email: raw.email,
+        password: raw.password,
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw._id),
+    );
   }
-
   static toPersistence(entity: User): UserModel {
     return {
+      _id: entity.id.toString(),
       name: entity.name,
       email: entity.email,
       password: entity.password,
--- src/modules/users/models/user.model.ts
+++ src/modules/users/models/user.model.ts
@@ -1,10 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @Prop({
+    type: mongoose.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+    required: false,
+    auto: true,
+  })
+  _id: string;
+
   @Prop({ required: true })
   name: string;
 
